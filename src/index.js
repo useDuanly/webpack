8dae8fed2d36a0d2aeb1b4ac0b57400ffba4a6c3
useDuanly <use.d.v.a.dev@gmail.com>
2022-03-21 03:28:08 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log("webpack-no-config");
+
+const numero = [1, 2, 3, 4, 5];
+
+const fun = () => {
+  numero.forEach((el) => console.log(...el));
+};
+fun();
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /.html$/i,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
